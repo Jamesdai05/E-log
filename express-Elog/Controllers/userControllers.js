f12aaa1dd2e6bdbb303df130fc3e94f29556a5ba
jamesdai05 <dai_guilong@hotmail.com>
2023-10-21 23:25:01 +0800

    add fetchallUsers

--- express-Elog/Controllers/userControllers.js
+++ express-Elog/Controllers/userControllers.js
@@ -1,1 +1,15 @@
 const userModel = require("../Models/User");
+
+const fetchAllUsers = async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Unable to fetch all users!" });
+  }
+};
+
+module.exports = {
+  fetchAllUsers,
+};
